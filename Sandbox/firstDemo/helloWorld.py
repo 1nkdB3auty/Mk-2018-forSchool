980cc9d3de20de134ebcdec06c10ee4ee66a7be7
maggi <inkdb3auty@gmail.com>
2018-10-08 13:47:01 -0300

    commented helloapp

--- Sandbox/firstDemo/helloWorld.py
+++ Sandbox/firstDemo/helloWorld.py
@@ -1,5 +1,12 @@
+#My first Python Application
+#Author: Maggi
+#Date: Oct. 8 2018
+
+# create a variable for the users name 
 def main():
     name = "Mike"
+
+    #output message to the user
     print("Hello, " + name + "!!")
 
 
